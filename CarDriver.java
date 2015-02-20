af153e049fa67b9b9f70f8d53cf49392d7dcffb3
Collin Mullis <cmullis@saintjoe.edu>
2015-02-20 15:47:11 -0500

    Yup, it's done.

---
+++ CarDriver.java
@@ -0,0 +1,11 @@
+public class CarDriver {
+	public static void main(String[] args) {
+	
+	Car ford = new Car("Ford", "Mustang", 2015);
+	System.out.println(ford.toString());
+
+	Car chevy = new Car("Chevy", "Camaro");
+	System.out.println(chevy.toString());
+
+}
+}	
